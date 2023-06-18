1a525801ad8810bc26f981ce506c1480fdbc4442
Mahesh <Maheshk2104@gmail.com>
2023-06-18 13:00:46 +0530

    [Mahesh]Add: Fibonacci Series

---
+++ FibnacciSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibnacciSeries
+    {
+        public void FibonacciSeries(int count)
+        {
+            int firstNumber = 0;
+            int secondNumber = 1;
+            Console.WriteLine("Fibonacci series: ");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(firstNumber);
+                int next = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FibnacciSeries fibnacciSeries = new FibnacciSeries();
+            fibnacciSeries.FibonacciSeries(10);
+
+            
+
+        }
+    }
+}
