bd7eb2b1bb2690eee5680c025b8863c6c64f992c
Mahesh <Maheshk2104@gmail.com>
2023-06-20 00:06:45 +0530

    [Mahesh]Add: Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        public void PrimeNum()
+        {
+            Console.WriteLine("Enter a Number");
+            int number = int.Parse(Console.ReadLine());
+            if(IsPrimeNumber(number))
+            {
+                Console.WriteLine(number + "is the prime number.");
+            }
+            else
+            {
+                Console.WriteLine(number + "is not a prime number");
+            }
+
+
+        }
+        public bool IsPrimeNumber(int number)
+        {
+            if(number < 2)
+            {
+                return false;
+            }
+            for(int divisor = 2; divisor <= number/2; divisor++)
+            {
+                if(number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace LogicalPrograms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -9,6 +9,9 @@
 
             PerfectNumber perfectNumbers = new PerfectNumber();
             perfectNumbers.perfectNumber();
+
+            PrimeNumber number = new PrimeNumber();
+            number.PrimeNum();
 
         }
     }
